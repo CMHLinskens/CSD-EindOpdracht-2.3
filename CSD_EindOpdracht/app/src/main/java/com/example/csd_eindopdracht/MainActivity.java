c0990908654660eab6510d170d1de008ab27e7d6
Conner Linskens <cmh.linskens@student.avans.nl>
2021-01-16 15:50:29 +0100

    Added LocationService, permission request and set up EventBus system

--- CSD_EindOpdracht/app/src/main/java/com/example/csd_eindopdracht/MainActivity.java
+++ CSD_EindOpdracht/app/src/main/java/com/example/csd_eindopdracht/MainActivity.java
@@ -1,14 +1,74 @@
 package com.example.csd_eindopdracht;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+
+import com.example.csd_eindopdracht.services.LocationService;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOGTAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Request for location permission
+        requestPermissionsIfNecessary(new String[]{
+                Manifest.permission.ACCESS_FINE_LOCATION,
+        });
+
+        // Subscribe to EventBus
+        EventBus.getDefault().register(this);
+        // Start locationService as an ForegroundService
+        Intent locationServiceIntent = new Intent(this, LocationService.class);
+        startForegroundService(locationServiceIntent);
+    }
+
+    /**
+     * Method triggered by EventBus when new location is received
+     * @param location new location data
+     */
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onLocationEvent(Location location){
+        Log.d(LOGTAG, "Latitude: " + location.getLatitude() + " Longitude: " + location.getLongitude());
+    }
+
+    /**
+     Checks for each permission if it already has been granted
+     if not ask the user for this permission
+     @param permissions String array with permissions to ask
+     */
+    private void requestPermissionsIfNecessary(String[] permissions) {
+        ArrayList<String> permissionsToRequest = new ArrayList<>();
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                // Permission is not granted
+                permissionsToRequest.add(permission);
+            }
+        }
+        if (permissionsToRequest.size() > 0) {
+            int REQUEST_PERMISSIONS_REQUEST_CODE = 1;
+            ActivityCompat.requestPermissions(
+                    this,
+                    permissionsToRequest.toArray(new String[0]),
+                    REQUEST_PERMISSIONS_REQUEST_CODE);
+        }
     }
 }
---
+++ CSD_EindOpdracht/app/src/main/java/com/example/csd_eindopdracht/services/LocationService.java
@@ -0,0 +1,94 @@
+package com.example.csd_eindopdracht.services;
+
+import android.app.Notification;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+
+import com.example.csd_eindopdracht.R;
+
+import org.greenrobot.eventbus.EventBus;
+
+public class LocationService extends Service {
+    private static final String LOGTAG = LocationService.class.getName();
+
+    private LocationManager locationManager = null;
+    private static final int LOCATION_INTERVAL = 1000;
+    private static final float LOCATION_DISTANCE = 10f;
+    LocationListener locationListener = null;
+
+    private class LocationListener implements android.location.LocationListener {
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+            EventBus.getDefault().post(location);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            // Ignore status changed
+        }
+
+        @Override
+        public void onProviderEnabled(@NonNull String provider) {
+            Log.e(LOGTAG, "onProviderEnabled: " + provider);
+        }
+
+        @Override
+        public void onProviderDisabled(@NonNull String provider) {
+            Log.e(LOGTAG, "onProviderDisabled: " + provider);
+        }
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        // Start notification for foreground service
+        Notification notification = new NotificationCompat.Builder(getApplicationContext(), "notifychannelid")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText("CultuurKompas Service Running ")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT).build();
+        startForeground(1, notification);
+
+        // initialise manager and listener
+        locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener();
+
+        // request updates from location manager
+        try {
+            locationManager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER, LOCATION_INTERVAL,
+                    LOCATION_DISTANCE, locationListener
+            );
+
+            locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER, LOCATION_INTERVAL,
+                    LOCATION_DISTANCE, locationListener
+            );
+        } catch (SecurityException e) {
+            Log.i(LOGTAG, "Failed to request location update", e);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(locationManager != null){
+            locationManager.removeUpdates(locationListener);
+        }
+    }
+}
