4a1b5cee32829ab115ef0d6cc297c22986415289
Bart <bartvanmoorsel7@gmail.com>
2021-01-16 20:21:30 +0100

    Added functionality to collectableRecyclerView

--- CSD_EindOpdracht/app/src/main/java/com/example/csd_eindopdracht/ui/CollectableAdapter.java
+++ CSD_EindOpdracht/app/src/main/java/com/example/csd_eindopdracht/ui/CollectableAdapter.java
@@ -1,4 +1,65 @@
 package com.example.csd_eindopdracht.ui;
 
-public class CollectableAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.csd_eindopdracht.R;
+import com.example.csd_eindopdracht.dataModel.collectable.Collectable;
+
+import java.util.ArrayList;
+
+public class CollectableAdapter extends RecyclerView.Adapter<CollectableAdapter.CollectableViewHolder> {
+    private final ArrayList<Collectable> collectables;
+    private OnItemClickListener onItemClickListener;
+
+    public interface OnItemClickListener{
+        void onItemClick(int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener){
+        this.onItemClickListener = listener;
+    }
+
+    public static class CollectableViewHolder extends RecyclerView.ViewHolder {
+//        public TextView itemNameTextView;
+        public CollectableViewHolder(@NonNull View itemView, OnItemClickListener onItemClickListener) {
+            super(itemView);
+//            itemNameTextView = itemView.findViewById(R.id.LampName);
+            itemView.setOnClickListener(v -> {
+                if(onItemClickListener != null){
+                    int position = getAdapterPosition();
+                    if (position != RecyclerView.NO_POSITION)
+                        onItemClickListener.onItemClick(position);
+                }
+            });
+        }
+    }
+
+    public CollectableAdapter(ArrayList<Collectable> collectables) {
+        this.collectables = collectables;
+    }
+
+    @NonNull
+    @Override
+    public CollectableViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_collectable_yugioh, parent, false);
+        return new CollectableViewHolder(view, onItemClickListener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CollectableViewHolder holder, int position) {
+        Collectable collectable = collectables.get(position);
+//        holder.itemNameTextView.setText(collectable.getName()); TODO: fix name, font and gravity
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.collectables.size();
+    }
+
 }
--- CSD_EindOpdracht/app/src/main/java/com/example/csd_eindopdracht/ui/fragment/InventoryFragment.java
+++ CSD_EindOpdracht/app/src/main/java/com/example/csd_eindopdracht/ui/fragment/InventoryFragment.java
@@ -8,15 +8,35 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.csd_eindopdracht.R;
+import com.example.csd_eindopdracht.dataModel.collectable.Collectable;
+import com.example.csd_eindopdracht.dataModel.collectable.YugiohCollectable;
+import com.example.csd_eindopdracht.ui.CollectableAdapter;
+
+import java.util.ArrayList;
 
 public class InventoryFragment extends Fragment {
+    private RecyclerView collectableRecyclerView;
+    private CollectableAdapter collectableAdapter;
+    private GridLayoutManager layoutManager;
+    private ArrayList<Collectable> inventory;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_inventory, container, false);
+
+        collectableRecyclerView = view.findViewById(R.id.collectable_recycler);
+        collectableRecyclerView.setHasFixedSize(true);
+        layoutManager = new GridLayoutManager(this.getContext(), 2);
+//        collectables = Data.INSTANCE.getInventory();
+        inventory.add(new YugiohCollectable("name gang", "card_placeholder.jpg", true)); // TEST PURPOSES
+
+        collectableRecyclerView.setLayoutManager(layoutManager);
+        collectableRecyclerView.setAdapter(collectableAdapter);
 
         return view;
     }
