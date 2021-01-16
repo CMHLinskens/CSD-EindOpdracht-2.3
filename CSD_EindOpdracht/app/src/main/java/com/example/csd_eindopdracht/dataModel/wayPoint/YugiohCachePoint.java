0e2aacfcd31f3bf09580a0ad51773cf9823d2f17
Bart <bartvanmoorsel7@gmail.com>
2021-01-16 14:47:49 +0100

    Added datamodels

---
+++ CSD_EindOpdracht/app/src/main/java/com/example/csd_eindopdracht/dataModel/wayPoint/YugiohCachePoint.java
@@ -0,0 +1,7 @@
+package com.example.csd_eindopdracht.dataModel.wayPoint;
+
+import com.example.csd_eindopdracht.dataModel.wayPoint.CachePoint;
+
+public class YugiohCachePoint extends CachePoint {
+
+}
