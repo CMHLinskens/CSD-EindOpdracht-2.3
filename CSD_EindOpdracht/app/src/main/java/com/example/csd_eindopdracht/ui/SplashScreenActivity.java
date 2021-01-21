6afbf9cf6231f7e5cd370a12ef737cac37916d7d
Conner Linskens <cmh.linskens@student.avans.nl>
2021-01-21 20:33:26 +0100

    Fixed crash bug on first start up

--- CSD_EindOpdracht/app/src/main/java/com/example/csd_eindopdracht/ui/SplashScreenActivity.java
+++ CSD_EindOpdracht/app/src/main/java/com/example/csd_eindopdracht/ui/SplashScreenActivity.java
@@ -1,11 +1,15 @@
 package com.example.csd_eindopdracht.ui;
 
 import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -14,6 +18,7 @@ import androidx.core.content.ContextCompat;
 import com.example.csd_eindopdracht.R;
 import com.example.csd_eindopdracht.services.LocationService;
 
+import java.security.Permission;
 import java.util.ArrayList;
 
 public class SplashScreenActivity extends AppCompatActivity {
@@ -25,20 +30,38 @@ public class SplashScreenActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_splash_screen);
 
+        // Create a notification channel for Android 8.1 and above
+        createNotificationChannel();
+
         // Request for location permission
         requestPermissionsIfNecessary(new String[]{
                 Manifest.permission.ACCESS_FINE_LOCATION,
         });
 
-        // Start locationService as a ForegroundService
-        Intent locationServiceIntent = new Intent(this, LocationService.class);
-        startForegroundService(locationServiceIntent);
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED) {
+            initialiseApp();
+        }
+    }
 
-        new Handler().postDelayed(() -> {
-            Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
-            SplashScreenActivity.this.startActivity(intent);
-            SplashScreenActivity.this.finish();
-        }, screenDuration);
+    /**
+     * Create a notification channel for Android 8.1 and above
+     * in order to avoid bad notification for startForeground
+     */
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "notifychannel";
+            String description = "notifydescription";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("notifychannelid", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
     /**
@@ -62,5 +85,33 @@ public class SplashScreenActivity extends AppCompatActivity {
                     permissionsToRequest.toArray(new String[0]),
                     REQUEST_PERMISSIONS_REQUEST_CODE);
         }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        requestPermissionsIfNecessary(new String[]{
+                Manifest.permission.ACCESS_FINE_LOCATION,
+        });
+        if(permissions[0].equals(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                initialiseApp();
+            }
+        }
+    }
+
+    /**
+     * Initialise the app
+     */
+    private void initialiseApp(){
+        // Start locationService as a ForegroundService
+        Intent locationServiceIntent = new Intent(this, LocationService.class);
+        startForegroundService(locationServiceIntent);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+            SplashScreenActivity.this.startActivity(intent);
+            SplashScreenActivity.this.finish();
+        }, screenDuration);
     }
 }
